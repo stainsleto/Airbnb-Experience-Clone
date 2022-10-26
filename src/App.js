9cf1f95ef6a1711464251c7d87ca6f16f5f0f2aa
Stains <stainsmault@gmail.com>
2022-10-26 23:25:55 +0530

    products

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import NavBar from './components/navbar'
 import Hero from './components/hero'
 import ProductSlider from './components/productSlider.js';
+import data from './components/data';
 
 function App() {
+
+  const cards = data.map( (item)=>{
+    return(
+      <ProductSlider 
+      key={item.id}
+      img ={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount = {item.stats.reviewCount}
+      country = {item.location}
+      title="Life lessons with Katie Zaferes"
+      price= {item.price}
+      />
+      
+    )
+  })
+
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <ProductSlider />
+      <section className="card-main">
+        {cards}
+      </section>
+      
+
     </div>
+    
   );
 }
 
--- src/components/productSlider.js
+++ src/components/productSlider.js
@@ -1,41 +1,23 @@
 import React from 'react';
-import ProductImage from '../images/image12.png';
-import Wedding from '../images/wedding.png';
 import Star from '../images/star.png';
-import Bike from '../images/bike.png';
 
-function ProductSlider(){
-    return(
-        <div className="product-main">
-            <div>
-                <div className="product-text">
-                    <img className="product-slider" src={ProductImage} />
-                    <p><img id="star-image" src={Star} width="17px"/>5.0 <span className="light-text">(6).USA</span></p>
-                    <p>Life lessons with Katie Zaferes</p>
-                    <p ><span className="bold-text">From $136</span>/ person</p>
-                </div>
+
+function ProductSlider(props){
+    return(  
+        <div>
+            <img className="product-slider" src= {`../images/${props.img}`} />
+            <div className="card-stats">
+                <img id="star-image" src={Star} width="17px"/>
+                <span>{props.rating}</span>
+                <span className="light-text">({props.reviewCount}).</span>
+                <span className="light-text">{props.country}</span>
             </div>
-
-            <div>
-                <div className="product-text">
-                    <img className="product-slider" src={Wedding} />
-                    <p><img id="star-image" src={Star} width="17px"/>5.0 <span className="light-text">(30).USA</span></p>
-                    <p>Learn wedding photography</p>
-                    <p ><span className="bold-text">From $125</span>/ person</p>
-                </div>
-            </div>
-
-            <div>
-                <div className="product-text">
-                    <img className="product-slider" src={Bike} />
-                    <p><img id="star-image" src={Star} width="17px"/>4.8 <span className="light-text">(2).USA</span></p>
-                    <p>Group Mountain Biking</p>
-                    <p ><span className="bold-text">From $125</span>/ person</p>
-                </div>
-            </div>
-
-
+            <p>{props.title}</p>
+            <p ><span className="bold-text">From ${props.price}</span>/ person</p>
+            
         </div>
+
+        
     )
 }
 
